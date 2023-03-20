a31da64915a4d7046416cd2ab4dff21b54a1589c
hollowdll <pixerchat@gmail.com>
2023-03-20 10:29:09 +0200

    change message model and add two format methods

--- src/main/java/com/example/app/controller/MessageController.java
+++ src/main/java/com/example/app/controller/MessageController.java
@@ -1,28 +1,22 @@
 package com.example.app.controller;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.app.model.AppUser;
 import com.example.app.model.Message;
+import com.example.app.model.MessageRoom;
 
 @Controller
 public class MessageController {
 
 	@GetMapping("/")
-	public String showMessage(Model model) {
+	public String showTestMessage(Model model) {
 		Message message = new Message(
-			"Test message",
-			0,
-			0,
-			LocalDateTime
-				.now(ZoneOffset.UTC)
-				.format(DateTimeFormatter.ofPattern("d/MM/uuuu HH:mm:ss"))
-				+ " UTC+0"
+			"Test message 123",
+			new AppUser(),
+			new MessageRoom()
 		);
 		
 		model.addAttribute("message", message);
--- src/main/java/com/example/app/model/Message.java
+++ src/main/java/com/example/app/model/Message.java
@@ -16,11 +16,11 @@ public class Message {
 		
 	}
 	
-	public Message(String text, AppUser sender, MessageRoom messageRoom, LocalDateTime created) {
+	public Message(String text, AppUser sender, MessageRoom messageRoom) {
 		this.text = text;
 		this.sender = sender;
 		this.messageRoom = messageRoom;
-		this.created = created;
+		created = LocalDateTime.now(ZoneOffset.UTC);
 	}
 
 	public Long getMessageId() {
@@ -61,6 +61,16 @@ public class Message {
 
 	public void setCreated(LocalDateTime created) {
 		this.created = created;
+	}
+	
+	// Format to day/month/year hours:minutes:seconds
+	public String formatCreated() {
+		return created.format(DateTimeFormatter.ofPattern("d/MM/uuuu HH:mm:ss"));
+	}
+	
+	// Format to day/month/year hours:minutes:seconds UTC+0
+	public String formatCreatedUTC() {
+		return created.format(DateTimeFormatter.ofPattern("d/MM/uuuu HH:mm:ss")) + " UTC+0";
 	}
 
 	@Override
