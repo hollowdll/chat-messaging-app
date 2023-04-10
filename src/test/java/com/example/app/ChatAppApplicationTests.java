315f1c6a2b29d92ca0e685c2f25579349da08147
hollowdll <pixerchat@gmail.com>
2023-04-10 15:02:15 +0300

    smoke testing

--- src/test/java/com/example/app/ChatAppApplicationTests.java
+++ src/test/java/com/example/app/ChatAppApplicationTests.java
@@ -8,18 +8,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.example.app.user.AppUserDAO;
+import com.example.app.message.MessageController;
+import com.example.app.messageroom.MessageRoomController;
+import com.example.app.user.UserController;
+import com.example.app.user.UserDetailsServiceImpl;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ChatAppApplicationTests {
 
+	private final UserController userController;
+	private final MessageRoomController messageRoomController;
+	private final MessageController messageController;
+	private final UserDetailsServiceImpl userDetailsService;
+	
 	@Autowired
-	private AppUserDAO appUserDao;
+	public ChatAppApplicationTests(
+		UserController userController,
+		MessageRoomController messageRoomController,
+		MessageController messageController,
+		UserDetailsServiceImpl userDetailsService
+	) {
+		this.userController = userController;
+		this.messageRoomController = messageRoomController;
+		this.messageController = messageController;
+		this.userDetailsService = userDetailsService;
+	}
 	
 	@Test
-	public void appUserDAOLoads() {
-		assertThat(appUserDao).isNotNull();
+	public void userControllerLoads() {
+		assertThat(userController).isNotNull();
+	}
+	
+	@Test
+	public void messageRoomControllerLoads() {
+		assertThat(messageRoomController).isNotNull();
+	}
+	
+	@Test
+	public void messageControllerLoads() {
+		assertThat(messageController).isNotNull();
+	}
+	
+	@Test
+	public void userDetailsServiceLoads() {
+		assertThat(userDetailsService).isNotNull();
 	}
 
 }
